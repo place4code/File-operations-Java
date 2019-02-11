47bf4d68e64362adfd059bc00c6671798f0d1145
place4code <place4code@gmail.com>
2019-02-11 18:21:47 +0100

    File read and write

---
+++ Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = null;
+        String filePath;
+        Scanner scanner = new Scanner(System.in);
+
+        //###############################################################
+        //################################         Find a file:
+        boolean fileFound = false;
+        while (!fileFound) {
+            System.out.println("Enter the path to the file in which you want to save the data: ");
+            filePath = scanner.nextLine();
+            if (filePath != null) {
+                file = new File(filePath);
+                if (file.exists()) {
+                    System.out.println("Path: " + filePath + " is correct");
+                    fileFound = true;
+                } else {
+                    System.out.println("Path: " + filePath + " is incorrect");
+                }
+            }
+
+        }
+        //###############################################################
+        //################################         Save to file:
+        FileWriter fileWriter = new FileWriter(file);
+
+        boolean endOfTyping = false;
+        while (!endOfTyping) {
+            System.out.println("Write something: ");
+            String content = scanner.nextLine();
+            if (content.equals("-")) {
+                endOfTyping = true;
+                continue;
+            }
+            System.out.println(content);
+            fileWriter.write(content + System.lineSeparator());
+        }
+        fileWriter.close();
+
+        //###############################################################
+        //################################         Read from a file:
+        int lineCounter = 0;
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            content.append(line);
+            content.append(System.lineSeparator());
+            lineCounter++;
+        }
+        bufferedReader.close();
+
+        System.out.println("Lines in the file: " + lineCounter);
+        System.out.println(content);
+
+
+
+
+
+
+
+
+    }
+}
